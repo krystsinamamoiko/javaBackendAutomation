5010b9d2f41f047a6cc61aaedb1d9967f9dc7fe3
krystsina.mamoiko <krystsina.mamoiko@gmail.com>
2022-11-28 11:01:30 +0300

    Lesson 04 Updates

--- src/main/java/ru/gb/backendautomation/model/complexSearch/Result.java
+++ src/main/java/ru/gb/backendautomation/model/complexSearch/Result.java
@@ -2,7 +2,6 @@ package ru.gb.backendautomation.model.complexSearch;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import ru.gb.backendautomation.model.complexSearch.Nutrition;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,26 +15,4 @@ public class Result {
     private Nutrition nutrition;
     @JsonIgnore
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-/*    @Test
-    void testComplexSearchQuery() {
-        String searchTitle = "pasta";
-        int randomOffset = new Random().nextInt(5);
-        List<Recipe> resultList = given()
-            .baseUri(getBaseUrl())
-            .param("apiKey", getApiKey())
-            .param("query", searchTitle)
-            .when()
-            .get("recipes/complexSearch")
-            .then()
-            .log().all()
-            .statusCode(200)
-            .extract().jsonPath().getList("results", Recipe.class);
-
-            Recipe[] recipes = given().when().get("person/").as(Recipe[].class);
-
-        for(Recipe recipe : resultList) {
-            assertThat(recipe.getTitle().toLowerCase().contains(searchTitle), is(true));
-        }
-    }*/
 }
